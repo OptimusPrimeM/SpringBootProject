1c39260cbd077beea1dc7b2f576cf4c7fad61ee3
Menik <menikdisney@gmail.com>
2018-08-08 11:58:15 +0600

    Initial commit

---
+++ Buliding a spring boot web app/initializer/FirstSpringBootEx/src/main/java/com/optimusprime/FirstSpringBootEx/FirstSpringBootExApplication.java
@@ -0,0 +1,12 @@
+package com.optimusprime.FirstSpringBootEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootExApplication.class, args);
+	}
+}
